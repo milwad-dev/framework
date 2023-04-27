dd20f30ed8dda0ca96d715a32514df8b12513ef9
miladev-ent <milwad.dev@gmail.com>
2023-04-28 00:18:12 +0330

    add `compileLimit` to CompilesStrings

--- src/Illuminate/View/Compilers/Concerns/CompilesStrings.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesStrings.php
@@ -4,5 +4,16 @@ namespace Illuminate\View\Compilers\Concerns;
 
 trait CompilesStrings
 {
-
+    /**
+     * Compile the stack statements into the content.
+     *
+     * @param  string  $value
+     * @param  int  $limit
+     * @param  string  $end
+     * @return string
+     */
+    protected function compileLimit($value, $limit = 100, $end = '...')
+    {
+        return "<?php echo str($value)->limit($limit, $end); ?>";
+    }
 }
